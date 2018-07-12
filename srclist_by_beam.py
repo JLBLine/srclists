3143f5b7a8c390fa940c52aef1f392a68ff380d0
JLBLine <jack.l.b.line@gmail.com>
2018-07-12 11:04:25 +1000

    Added better protection if there are zero fluxes in srclist

--- srclist_by_beam.py
+++ srclist_by_beam.py
@@ -213,6 +213,9 @@ def create_source(prim_name=None, prim_ra=None, prim_dec=None, offset=None, prim
             
     ##For each set of source infomation, calculate and extrapolated flux at the centra flux value
     for freqs,fluxs in zip(source.freqs,source.fluxs):
+        
+        fluxs = array(fluxs)[where(array(fluxs) > 0.0)]
+        freqs = array(freqs)[where(array(fluxs) > 0.0)]
 
         ##If only one freq, extrap with an SI of -0.8:
         if len(freqs)==1:
