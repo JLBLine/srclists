b461135a38f1df7d70f1a47743aac34456ae264f
NickSwainston <nickaswainston@gmail.com>
2019-10-24 13:00:23 +0800

    Made a setup.py so it can be used to test the script by comparing previous output files with the current ones

---
+++ setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+"""
+Setup for srclists
+"""
+import os
+from setuptools import setup
+
+
+def read(fname):
+    """Read a file"""
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name="srclists",
+      url="https://github.com/JLBLine/srclists",
+      #description="",
+      long_description=read('README.md'),
+      scripts=['srclist_by_beam.py'],
+      data_files=['srclist_pumav3_EoR0aegean_EoR1pietro+ForA_phase1+2.txt',
+                  'srclist_pumav3_EoR0aegean_EoR1pietro+ForA_TGSSgalactic.txt',
+                  'srclist_puma-v2_complete.txt',
+                  'srclist_pumav3_EoR0aegean_EoR1pietro+ForA.txt'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest']
+)
+
